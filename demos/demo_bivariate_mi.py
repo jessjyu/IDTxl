55038146fad4ab6a487a61e3e28e657f41c73898
Jess Yu <jessjinyu@outlook.com>
2024-03-22 22:23:35 +0000

    22March

--- demos/demo_bivariate_mi.py
+++ demos/demo_bivariate_mi.py
@@ -15,6 +15,8 @@ settings = {'cmi_estimator': 'JidtGaussianCMI',
             'max_lag_sources': 5,
             'min_lag_sources': 1}
 
+
+# %% 
 # c) Run analysis
 results = network_analysis.analyse_network(settings=settings, data=data)
 
